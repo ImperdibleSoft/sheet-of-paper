736cdcedb6695514e3fd54e84078fd9bac551044
Rafael Pérez García <info@imperdiblesoft.com>
2015-07-26 19:36:47 -0300

    File adding

---
+++ sheet-of-paper.js
@@ -0,0 +1,50 @@
+﻿var alreadyPrinted = false;
+
+function startPrinting(){
+	alreadyPrinted = false;
+	
+	/*	Set the viewer in Printer mode	*/
+	document.body.className = "printer";
+	
+	/*	Hide all invisible elemenets	*/
+	var invisibleItems = document.getElementsByClassName("invisible");
+	for(var x in invisibleItems){
+		if(invisibleItems[x] && invisibleItems[x].style){
+			invisibleItems[x].style.display = "none";
+		}
+	}
+	
+	/*	Open Print dialog	*/
+	if(window.print()){ 
+		alreadyPrinted = true; 
+	}else{ 
+		alreadyPrinted = true;
+	}
+	
+	function checkAlreadyPrinted(){
+		if(alreadyPrinted == true){
+			restoreViewer();
+		}
+		else{
+			setTimeout(function(){
+				checkAlreadyPrinted();
+			}, 100);
+		}
+	}
+	checkAlreadyPrinted();
+}
+
+function restoreViewer(){
+	alreadyPrinted = false;
+	
+	/*	Set the viewer in Printer mode	*/
+	document.body.className = "viewer";
+	
+	/*	Hide all invisible elemenets	*/
+	var invisibleItems = document.getElementsByClassName("invisible");
+	for(var x in invisibleItems){
+		if(invisibleItems[x] && invisibleItems[x].style){
+			invisibleItems[x].style.display = "block";
+		}
+	}
+}
